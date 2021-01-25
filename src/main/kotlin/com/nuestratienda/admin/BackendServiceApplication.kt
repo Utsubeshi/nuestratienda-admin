47edafc4205253d0d95a7efbabb1da1e79d97d92
Juan Niño <jcns.teknic@gmail.com>
2021-01-25 13:06:15 -0500

    pruebas

--- src/main/kotlin/com/nuestratienda/admin/BackendServiceApplication.kt
+++ src/main/kotlin/com/nuestratienda/admin/BackendServiceApplication.kt
@@ -2,8 +2,10 @@ package com.nuestratienda.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class BackendServiceApplication
@@ -14,8 +16,9 @@ fun main(args: Array<String>) {
 
 @RestController
 class HolaMundo {
-
     @GetMapping("/")
     fun Saludo(): String = "Hola"
 
 }
+
+
