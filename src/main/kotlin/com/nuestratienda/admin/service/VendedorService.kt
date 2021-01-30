9ab93a577278ce08e9ed2fe220fea71eedaec1dd
Juan Niño <jcns.teknic@gmail.com>
2021-01-30 18:12:09 -0500

    corriengo las respuestas del registro - VendedorService.saveNewUser5

--- src/main/kotlin/com/nuestratienda/admin/service/VendedorService.kt
+++ src/main/kotlin/com/nuestratienda/admin/service/VendedorService.kt
@@ -68,4 +68,8 @@ class RespuestaCulqui(
     var isSuccessful: Boolean = false,
     var mensaje: String = "",
     var id: String = ""
-){}
+){
+    override fun toString(): String {
+        return "RespuestaCulqui(isSuccessful=$isSuccessful, mensaje='$mensaje', id='$id')"
+    }
+}
