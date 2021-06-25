1c931ef7d9af2ae32e5be1beef2de23e816d2325
Juan Niño <jcns.teknic@gmail.com>
2021-06-25 15:41:16 -0500

    [Fix] configurando la pagina de error del servicio3

--- src/main/kotlin/com/nuestratienda/admin/AppConfiguration.kt
+++ src/main/kotlin/com/nuestratienda/admin/AppConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
 
 @Configuration
-@EnableSwagger2WebMvc
 open class AppConfiguration {
     @Bean
     open fun bCryptPasswordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
