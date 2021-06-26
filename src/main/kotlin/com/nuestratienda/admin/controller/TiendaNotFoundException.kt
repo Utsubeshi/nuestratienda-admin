56b02abb10505fd0087c086be18c4ec85c5217a8
Juan Niño <jcns.teknic@gmail.com>
2021-06-26 11:51:23 -0500

    [Fix] prubando un manejador de excepciones para Tienda Controller2

--- src/main/kotlin/com/nuestratienda/admin/controller/TiendaNotFoundException.kt
+++ src/main/kotlin/com/nuestratienda/admin/controller/TiendaNotFoundException.kt
@@ -3,6 +3,6 @@ package com.nuestratienda.admin.controller
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Registro no encontrado")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Registro no encontrado")
 class TiendaNotFoundException : Exception() {
 }
