9e94c2559d737ab12771e0703ab2679086eec2f5
Juan Niño <jcns.teknic@gmail.com>
2021-06-26 19:53:35 -0500

    asdfg

--- src/main/kotlin/com/nuestratienda/admin/controller/TiendaController.kt
+++ src/main/kotlin/com/nuestratienda/admin/controller/TiendaController.kt
@@ -25,8 +25,9 @@ class TiendaController (
     @GetMapping("/{idTienda}", produces = arrayOf("application/json"))
     fun getStoreById(@PathVariable("idTienda") idTienda: Long) : ResponseEntity<Any> {
         try {
-            val tienda: Tienda = service.getStoreById(idTienda) ?: return ResponseEntity("Registro no encontrado",HttpStatus.NOT_FOUND)
-            return ResponseEntity(tienda, HttpStatus.OK)
+//            val tienda: Tienda = service.getStoreById(idTienda) ?: return ResponseEntity("Registro no encontrado",HttpStatus.NOT_FOUND)
+//            return ResponseEntity(tienda, HttpStatus.OK)
+            throw ApiRequestException("oops")
         } catch (ex: NullPointerException) {
             throw ApiRequestException("oops")
         }
