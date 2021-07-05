699ca9b3b6df22bf64ef556547e034322a802be9
Juan Niño <jcns.teknic@gmail.com>
2021-07-05 13:16:42 -0500

    [FIX]agregando validacion de tipo de dato en el modelo Vendedo/nombres5

--- src/main/kotlin/com/nuestratienda/admin/repository/VendedorRepository.kt
+++ src/main/kotlin/com/nuestratienda/admin/repository/VendedorRepository.kt
@@ -1,6 +1,7 @@
 package com.nuestratienda.admin.repository
 
 import com.nuestratienda.admin.model.Vendedor
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param
 interface VendedorRepository : CrudRepository<Vendedor, Long> {
     fun findByCorreo(email: String): Vendedor?
 
+    @Modifying
     @Query("update Vendedor v set v.nombres = :nombres, v.apellidos = :apellidos where v.id = :id ")
     fun updateUser(@Param(value = "nombres") nombres: String,
                    @Param(value = "apellidos") apellidos: String,
