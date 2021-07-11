36e2553406d9cf5b1917414cc34ea7ca5643067f
Juan Niño <jcns.teknic@gmail.com>
2021-07-11 00:09:04 -0500

    dehabilite el SecurityAutoConfiguration

--- src/main/kotlin/com/nuestratienda/admin/BackendServiceApplication.kt
+++ src/main/kotlin/com/nuestratienda/admin/BackendServiceApplication.kt
@@ -2,9 +2,10 @@ package com.nuestratienda.admin
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 open class BackendServiceApplication
 
 fun main(args: Array<String>) {
